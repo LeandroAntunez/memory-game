9ed61db7d37866960d7e67887d9edac14d5683e9
Leandro Antunez <leandro_antunez@outlook.com>
2022-02-11 15:59:38 -0300

    Title, subtitle added. Try text fixed.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".card"),
-timeTag = document.querySelector(".time b"),
-flipsTag = document.querySelector(".flips b"),
-refreshBtn = document.querySelector(".details button");
+    timeTag = document.querySelector(".time b"),
+    flipsTag = document.querySelector(".flips b"),
+    refreshBtn = document.querySelector(".details button");
 
 let maxTime = 20;
 let timeLeft = maxTime;
@@ -12,37 +12,37 @@ let isPlaying = false;
 let cardOne, cardTwo, timer;
 
 function initTimer() {
-    if(timeLeft <= 0) {
+    if (timeLeft <= 0) {
         return clearInterval(timer);
     }
     timeLeft--;
     timeTag.innerText = timeLeft;
 }
 
-function flipCard({target: clickedCard}) {
-    if(!isPlaying) {
+function flipCard({ target: clickedCard }) {
+    if (!isPlaying) {
         isPlaying = true;
         timer = setInterval(initTimer, 1000);
     }
-    if(clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
-        flips++;
-        flipsTag.innerText = flips;
+    if (clickedCard !== cardOne && !disableDeck && timeLeft > 0) {
         clickedCard.classList.add("flip");
-        if(!cardOne) {
+        if (!cardOne) {
             return cardOne = clickedCard;
         }
         cardTwo = clickedCard;
         disableDeck = true;
         let cardOneImg = cardOne.querySelector(".back-view img").src,
-        cardTwoImg = cardTwo.querySelector(".back-view img").src;
+            cardTwoImg = cardTwo.querySelector(".back-view img").src;
         matchCards(cardOneImg, cardTwoImg);
     }
 }
 
 function matchCards(img1, img2) {
-    if(img1 === img2) {
+    flips++;
+    flipsTag.innerText = flips;
+    if (img1 === img2) {
         matchedCard++;
-        if(matchedCard == 6 && timeLeft > 0) {
+        if (matchedCard == 6 && timeLeft > 0) {
             return clearInterval(timer);
         }
         cardOne.removeEventListener("click", flipCard);
